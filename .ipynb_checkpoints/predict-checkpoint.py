1c375643e59f23dedabd224db0f32f29cb25f626
yifan <ywu3939@gmail.com>
2024-06-30 22:44:39 -0500

    change the predict.py output csv to p-value

--- .ipynb_checkpoints/predict-checkpoint.py
+++ .ipynb_checkpoints/predict-checkpoint.py
@@ -4,7 +4,18 @@ from tensorflow.keras.models import load_model
 import joblib
 import os
 
-def mc_dropout_predictions_class(model, inputs, label_encoder, n_samples=100):
+
+
+############################################
+#number of replication, default=200
+n = 200
+#input data folder
+folder_path = "cgc_input_reformat"
+############################################
+
+
+
+def mc_dropout_predictions_class(model, inputs, label_encoder, n_samples=200):
     predictions = []
     for _ in range(n_samples):
         pred_output = model(inputs, training=True)  # Dropout active during inference
@@ -14,8 +25,10 @@ def mc_dropout_predictions_class(model, inputs, label_encoder, n_samples=100):
         
     predictions = np.stack(predictions)
     return predictions
-
+############################################
 # Load the saved model
+############################################
+
 model_dl = load_model('trained_model.h5')
 le = joblib.load('label_encoder.pkl')
 
@@ -23,7 +36,6 @@ le = joblib.load('label_encoder.pkl')
 #Import all csv file under Train_data folder
 ############################################
 
-folder_path = "cgc_input_reformat"
 # List to hold DataFrames
 dataframes = []
 # Loop through all files in the folder
@@ -37,10 +49,13 @@ combined_data = pd.concat(dataframes, ignore_index=True)
 combined_data.to_csv('combined_predict_data.csv', index=False)
 file_path = os.path.join('combined_predict_data.csv')
 data = pd.read_csv(file_path)
-
 test_seqs = np.array([test_item.replace("|", ",").replace(",", " ") for test_item in data["sequence"].values])
 label = data['cgc_id']
-result = mc_dropout_predictions_class(model_dl, test_seqs, le, )
+
+############################################
+#Run the prediction model
+############################################
+result = mc_dropout_predictions_class(model_dl, test_seqs, le, n)
 t_result = result.T
 unique_numbers = np.unique(t_result)  # Get the unique numbers in the array
 
@@ -52,9 +67,15 @@ for i, row in enumerate(t_result):
     counts = np.array([np.sum(row == num) for num in unique_numbers])
     counts_by_row[i] = counts
 
-# Print the counts
-# print(counts_by_row)
+
 result = pd.DataFrame(counts_by_row, columns = unique_numbers)
 result.insert(0, 'ID', label)
-result.to_csv('predictions_counts.csv', index=False)
-print(f"Prediction Output saved as predictions_counts.csv")
+
+
+column_name = result.columns.values[1:]
+result_melt = pd.melt(result, id_vars='ID', value_vars=column_name, var_name = 'substrate', value_name = 'p-value')
+result_melt['p-value'] = result_melt['p-value']/n
+result_melt_sort = result_melt.sort_values(by=['ID', 'p-value'], ascending = False)
+
+result_melt_sort.to_csv('predictions_results.csv', index=False)
+print(f"Prediction Output saved as predictions_results.csv")
